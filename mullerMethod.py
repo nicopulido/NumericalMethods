990f87164228fc8a98be1415634897ef6a096f76
nicopulido <nicopipe2010@gmail.com>
2025-04-20 18:03:23 -0500

    fixed some miscalculations and bugs of math error

--- mullerMethod.py
+++ mullerMethod.py
@@ -37,30 +37,32 @@ def realFunction(function, x_0, x_1, x_2, givenError):
 
     h_0 = x_1 - x_0
     h_1 = x_2 - x_1
-    d_0 = (function(x_1) - function(x_0))/(x_1 - x_0)
-    d_1 = (function(x_2) - function(x_1))/(x_2 - x_1)
+    d_0 = (function(x_1) - function(x_0))/(h_0)
+    d_1 = (function(x_2) - function(x_1))/(h_1)
     a = (d_1 - d_0)/(h_0 + h_1)
     b = a * h_1 + d_1
     c = function(x_2)
-    i = 0
+    i = 1
+    x_3 = 0
 
     givenError = math.ceil((1/math.log(2)) * math.log((x_2 - x_0)/givenError))
 
-    while((b**2 > 4*a*c) and (i < givenError)):
+    discriminant = math.sqrt(b**2 - 4*a*c)
+
+    if abs(discriminant + b) < abs(b - discriminant):
+        discriminant = - discriminant
+
+    x_3 = x_2 + (-2 * c)/(b + discriminant)
+
+    print(f"{(i):<10}{x_0:<10.5f}{x_1:<10.5f}{x_2:<10.5f}{h_0:<10.5f}{h_1:<10.5f}{function(x_0):<10.5f}{function(x_1):<10.5f}{function(x_2):<10.5f}{d_0:<10.5f}{d_1:<10.5f}{a:<10.5f}{b:<10.5f}{c:<10.5f}{x_3:<10.5f}")
+    
+    x_0 = x_1
+    x_1 = x_2
+    x_2 = x_3
+
+    while(b**2 > 4*a*c) and (i < givenError) and (x_2 - x_1 !=0) and (x_1 - x_0 !=0):
         i += 1
 
-        discriminant = math.sqrt(b**2 - 4*a*c)
-
-        if abs(discriminant + b) < abs(discriminant - b):
-            discriminant = - discriminant
-
-        x_3 = (-2 * c)/(b + discriminant)
-
-        print(f"{(i+1):<10}{x_0:<10.5f}{x_1:<10.5f}{x_2:<10.5f}{h_0:<10.5f}{h_1:<10.5f}{function(x_0):<10.5f}{function(x_1):<10.5f}{function(x_2):<10.5f}{d_0:<10.5f}{d_1:<10.5f}{a:<10.5f}{b:<10.5f}{c:<10.5f}{x_3:<10.5f}")
-
-        x_0 = x_1
-        x_1 = x_2
-        x_2 = x_3
 
         h_0 = x_1 - x_0
         h_1 = x_2 - x_1
@@ -69,6 +71,19 @@ def realFunction(function, x_0, x_1, x_2, givenError):
         a = (d_1 - d_0)/(h_0 + h_1)
         b = a * h_1 + d_1
         c = function(x_2)
+
+        discriminant = math.sqrt(b**2 - 4*a*c)
+
+        if abs(discriminant + b) < abs(b - discriminant):
+            discriminant = - discriminant
+
+        x_3 = x_2 + (-2 * c)/(b + discriminant)
+
+        print(f"{(i):<10}{x_0:<10.5f}{x_1:<10.5f}{x_2:<10.5f}{h_0:<10.5f}{h_1:<10.5f}{function(x_0):<10.5f}{function(x_1):<10.5f}{function(x_2):<10.5f}{d_0:<10.5f}{d_1:<10.5f}{a:<10.5f}{b:<10.5f}{c:<10.5f}{x_3:<10.5f}")
+
+        x_0 = x_1
+        x_1 = x_2
+        x_2 = x_3
 
 
 
