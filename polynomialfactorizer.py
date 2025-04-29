e3930a05dcdc06abb3a5f20348e6963323b5349b
nicopulido <nicopipe2010@gmail.com>
2025-04-29 13:08:45 -0500

    actually working but still buggy

--- polynomialfactorizer.py
+++ polynomialfactorizer.py
@@ -1,27 +1,37 @@
-from sympy import symbols, Eq,sin, cos,log,exp,diff, Poly, lambdify
+from sympy import symbols, Poly 
 import math
 
-def sinteticDivision(polinomial, x, Xn):
-    derivatedFunction = diff(polinomial(x), x)
-    Xn_1 = Xn - polinomial(Xn)/derivatedFunction(Xn)
-    while(derivatedFunction(Xn_1) != 0):
-        Xn = Xn_1
-        Xn_1 = Xn - polinomial(Xn)/derivatedFunction(Xn)
-    return Xn_1
-
-def reducePolinomial(polinomial, x, Xn):
-    derivatedFunction = diff(polinomial(x), x)
 
 
+def sinteticDivision(polynomial, x, Xn):
+    derivatedPoly = polynomial.diff(x)
+    Xn = Xn - polynomial.subs(x, Xn)/derivatedPoly.subs(x, Xn)
+    while(derivatedPoly(Xn) != 0):
+        Xn = Xn - polynomial.subs(x, Xn)/derivatedPoly.subs(x, Xn)
+    return Xn
 
-
-
-
+def reducePolynomial(polyCoefficients, Xn):
+    reducedPolynomial = [polyCoefficients[0]]
+    for i in range(1, len(polyCoefficients) - 1):
+        reducedPolynomial.append(reducedPolynomial[-1] * Xn + polyCoefficients[i])
+    return reducedPolynomial
 
 x = symbols('x')
 
-polinomial = lambdify(Poly(x, input("Ingrese una función: ")))
-givenError = float(input("Ingrese el error esperado: "))
+polynomial = Poly(input("Ingrese el polinomio: "), x)
+
+while(not polynomial.is_irreducible):
+    Xn = sinteticDivision(polynomial, x, 0)
+
+    polyCoefficients = polynomial.all_coeffs()
+    reducedPolynomial = reducePolynomial(polyCoefficients, x, Xn)
+
+    polynomial = Poly(reducedPolynomial, x)
+
+print(polynomial.as_expr())
+    
+      
 
 
-print(Poly(polinomial(x), x).all_coeffs())
+
+
