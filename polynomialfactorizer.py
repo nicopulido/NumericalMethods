faee3348afcfa34f893e2046e4801867568f9865
nicopulido <nicopipe2010@gmail.com>
2025-05-01 10:54:45 -0500

    it works, i resolved some printing issues and some issues with sintetic division definition

--- polynomialfactorizer.py
+++ polynomialfactorizer.py
@@ -3,14 +3,18 @@ import math
 
 
 
-def sinteticDivision(polynomial, x, Xn):
+def sinteticDivision(polynomial, x,):
     derivatedPoly = polynomial.diff(x)
-    while(polynomial.subs(x,Xn) != 0):
-        print(derivatedPoly.subs(x, Xn))
-        print(polynomial.subs(x, Xn))
-        print(Xn)
-        Xn = Xn - polynomial.subs(x, Xn)/derivatedPoly.subs(x, Xn)
-    return Xn
+
+    # Iterate over possible values for Xn from 1 to 100 and their negatives
+    for i in range(1, 101):
+        for Xn in [i, -i]:  # Test both positive and negative values
+            if polynomial.subs(x, Xn) == 0:  # Check if Xn is a root
+                # Use the while loop to refine the root
+                while polynomial.subs(x, Xn) != 0:
+                    print("Xn evaluado en el polinomio: " + str(polynomial.subs(x, Xn)))
+                    Xn = Xn - polynomial.subs(x, Xn) / derivatedPoly.subs(x, Xn)
+                return Xn 
 
 def reducePolynomial(polyCoefficients, Xn):
     reducedPolynomial = [polyCoefficients[0]]
@@ -22,17 +26,25 @@ x = symbols('x')
 
 polynomial = Poly(input("Ingrese el polinomio: "), x)
 
-while(not polynomial.is_irreducible):
-    Xn = sinteticDivision(polynomial, x, 1)
+print(polynomial.is_irreducible)
 
-    print("(" + "x" +  str(-Xn) + ")")
+while(not polynomial.is_irreducible):
+
+    Xn = sinteticDivision(polynomial, x)
+
+    if(Xn == 0):
+        print("x", end="")
+    elif(Xn > 0):
+        print("(" + "x - " +  str(Xn) + ")", end="")
+    else:
+        print("(" + "x + " +  str(-Xn) + ")", end="")
 
     polyCoefficients = polynomial.all_coeffs()
     reducedPolynomial = reducePolynomial(polyCoefficients, Xn)
 
     polynomial = Poly(reducedPolynomial, x)
 
-print(polynomial.as_expr())
+print("(" + str(polynomial.as_expr()) + ")")
     
       
 
