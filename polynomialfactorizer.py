34e271c54a7c36208efedaf8487434e24f3d7703
nicopulido <nicopipe2010@gmail.com>
2025-05-01 12:18:18 -0500

    Actually working how i want, just left printing some charts to follow the method

--- polynomialfactorizer.py
+++ polynomialfactorizer.py
@@ -9,11 +9,11 @@ def sinteticDivision(polynomial, x,):
     # Iterate over possible values for Xn from 1 to 100 and their negatives
     for i in range(1, 101):
         for Xn in [i, -i]:  # Test both positive and negative values  # Check if Xn is a root     # Use the while loop to refine the root                print(Xn)
-            while abs(polynomial.subs(x, Xn)) > 1e-10:
-                print("Xn evaluado en el polinomio: " + str(round(polynomial.subs(x, Xn), 5)))
-                print(round(Xn, 5))
+            while abs(polynomial.subs(x, Xn)) > 1e-20:
                 Xn = Xn - polynomial.subs(x, Xn) / derivatedPoly.subs(x, Xn)
-            return Xn 
+
+            if(polynomial.subs(x, int(round(Xn))) == 0):
+                return Xn
 
 def reducePolynomial(polyCoefficients, Xn):
     reducedPolynomial = [polyCoefficients[0]]
@@ -30,6 +30,9 @@ while(not polynomial.is_irreducible):
 
     Xn = sinteticDivision(polynomial, x)
 
+    if abs(Xn - round(Xn)) < 1e-10:
+        Xn = int(round(Xn))
+
     if(Xn == 0):
         print("x", end="")
     elif(Xn > 0):
@@ -40,7 +43,13 @@ while(not polynomial.is_irreducible):
     polyCoefficients = polynomial.all_coeffs()
     reducedPolynomial = reducePolynomial(polyCoefficients, Xn)
 
+    reducedPolynomial = [
+        int(c) if abs(c - round(c)) < 1e-20 else c for c in reducedPolynomial
+    ]
+
+
     polynomial = Poly(reducedPolynomial, x)
+
     
 print("(" + str(polynomial.as_expr().evalf(5)) + ")")
     
