5d099412141d09430491cfaa4b063020a1b62a93
nicopulido <nicopipe2010@gmail.com>
2025-05-01 11:34:55 -0500

    it somehow works as i want but there are some issues with de is_irreducible value

--- polynomialfactorizer.py
+++ polynomialfactorizer.py
@@ -8,13 +8,12 @@ def sinteticDivision(polynomial, x,):
 
     # Iterate over possible values for Xn from 1 to 100 and their negatives
     for i in range(1, 101):
-        for Xn in [i, -i]:  # Test both positive and negative values
-            if polynomial.subs(x, Xn) == 0:  # Check if Xn is a root
-                # Use the while loop to refine the root
-                while polynomial.subs(x, Xn) != 0:
-                    print("Xn evaluado en el polinomio: " + str(polynomial.subs(x, Xn)))
-                    Xn = Xn - polynomial.subs(x, Xn) / derivatedPoly.subs(x, Xn)
-                return Xn 
+        for Xn in [i, -i]:  # Test both positive and negative values  # Check if Xn is a root     # Use the while loop to refine the root                print(Xn)
+            while abs(polynomial.subs(x, Xn)) > 1e-10:
+                print("Xn evaluado en el polinomio: " + str(round(polynomial.subs(x, Xn), 5)))
+                print(round(Xn, 5))
+                Xn = Xn - polynomial.subs(x, Xn) / derivatedPoly.subs(x, Xn)
+            return Xn 
 
 def reducePolynomial(polyCoefficients, Xn):
     reducedPolynomial = [polyCoefficients[0]]
@@ -22,11 +21,10 @@ def reducePolynomial(polyCoefficients, Xn):
         reducedPolynomial.append(reducedPolynomial[-1] * Xn + polyCoefficients[i])
     return reducedPolynomial
 
+
 x = symbols('x')
 
 polynomial = Poly(input("Ingrese el polinomio: "), x)
-
-print(polynomial.is_irreducible)
 
 while(not polynomial.is_irreducible):
 
@@ -35,16 +33,16 @@ while(not polynomial.is_irreducible):
     if(Xn == 0):
         print("x", end="")
     elif(Xn > 0):
-        print("(" + "x - " +  str(Xn) + ")", end="")
+        print("(" + "x - " +  str(round(Xn, 5)) + ")", end="")
     else:
-        print("(" + "x + " +  str(-Xn) + ")", end="")
+        print("(" + "x + " +  str(round(-Xn, 5))  + ")", end="")
 
     polyCoefficients = polynomial.all_coeffs()
     reducedPolynomial = reducePolynomial(polyCoefficients, Xn)
 
     polynomial = Poly(reducedPolynomial, x)
-
-print("(" + str(polynomial.as_expr()) + ")")
+    
+print("(" + str(polynomial.as_expr().evalf(5)) + ")")
     
       
 
